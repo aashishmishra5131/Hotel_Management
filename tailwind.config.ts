7629b1cc94aaa18f8f3a328d9424dfd9e4307ced
AashishMishra <aashishmishra5131@gmail.com>
2025-01-04 15:39:28 +0530

    Hotel Management

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,28 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
+const { fontFamily } = require('tailwindcss/defaultTheme');
 
-export default {
+const config: Config = {
+  darkMode: 'class',
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: '#038C7F',
+        secondary: '#F2C641',
+        tertiary: {
+          dark: '#F27405',
+          light: '#F2C641',
+        },
+      },
+      fontFamily: {
+        poppins: ['var(--font-poppins)', ...fontFamily.sans],
       },
     },
   },
   plugins: [],
-} satisfies Config;
+};
+export default config;
